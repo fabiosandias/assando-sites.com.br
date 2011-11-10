743ecdb2e8d2671d7b709e6c915df868317fe9cd
Thiago Belem <contato@thiagobelem.net>
2011-11-10 20:46:32 -0200

    Correção no Status do pagamento e cronjob para atualizar os pagamentos

--- Controller/CronjobsController.php
+++ Controller/CronjobsController.php
@@ -48,4 +48,76 @@ class CronjobsController extends AppController {
 		$this->EmailQueue->processQueue(); exit;
 	}
 	
+	/**
+	 * Atualiza os pagamentos do PagSeguro
+	 */
+	public function syncPagSeguro() {
+		$this->loadModel('Payment');
+		
+		// Importa o vendor PagSeguroLibrary
+		App::import('Vendor', 'PagSeguro', array('file' => 'PagSeguroLibrary' . DS . 'PagSeguroLibrary.php'));
+		
+		// Objeto de credenciais
+		$AccountCredentials = new AccountCredentials(Configure::read('PagSeguro.API.email'), Configure::read('PagSeguro.API.token'));
+		
+		$initialDate = date('c', strtotime('-1 month'));
+		$finalDate = date('c');
+		
+		$pageNumber = 1;
+		$maxPageResults = 30;
+		
+		$Transactions = TransactionSearchService::searchByDate($AccountCredentials, $initialDate, $finalDate, $pageNumber, $maxPageResults);
+		
+		foreach ($Transactions->getTransactions() AS $Transaction) {
+			$reference = $Transaction->getReference();
+			
+			// Se não tem referência, pula
+			if (empty($reference))
+				continue;
+			
+			// Encontra o aluno
+			$Student = $this->Payment->Student->field('id', array('SHA1(Student.email)' => $reference));
+			
+			// Se não tem aluno, pula
+			if (empty($Student))
+				continue;
+			
+			// Enconta o pagamento pelo código e pelo ID do aluno (que corresponde ao reference)
+			$Payment = $this->Payment->find('first', array(
+				'fields' => array('id', 'updated'),
+				'conditions' => array(
+					'code' => $Transaction->getCode(),
+					'student_id' => $Student
+				)
+			));
+			
+			// Se encontrou o pagamento e ele está atualizado
+			if (!empty($Payment) && (strtotime($Payment['Payment']['updated']) == strtotime($Transaction->getLastEventDate())))
+				continue;
+			
+			$data = array(
+				'id' => !empty($Payment) ? $Payment['Payment']['id'] : null,
+				'student_id' => !empty($Student) ? $Student : 0,
+	
+				'code' => $Transaction->getCode(),
+				'reference' => !empty($reference) ? $reference : '',
+	
+				'value' => $Transaction->getGrossAmount(),
+				'datetime' => $Transaction->getDate(),
+	
+				'payment_gateway_id' => PAYMENT_GATEWAY_PAGSEGURO,
+				'payment_method_id' => $Transaction->getPaymentMethod()->getType()->getValue(),
+				
+				'status_id' => max(11, 10 + $Transaction->getStatus()->getValue()),
+				
+				'updated' => $Transaction->getLastEventDate()
+			);
+			
+			// Salva ou atualiza os dados do pagamento
+			$this->Payment->save($data);
+		}
+		
+		exit;
+	}
+	
 }
