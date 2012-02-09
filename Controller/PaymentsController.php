04bb5d250ac7f31545b6cafa4667abb6426e3c7c
Thiago Belem <contato@thiagobelem.net>
2012-02-09 18:55:54 -0200

    Inicio da notificação do PagSeguro

--- Controller/PaymentsController.php
+++ Controller/PaymentsController.php
@@ -16,6 +16,16 @@ App::uses('AppController', 'Controller');
 class PaymentsController extends AppController {
 		
 	/**
+	 * Antes de filtar
+	 */
+	public function beforeFilter() {
+		if ($this->action == 'syncPayment')
+			$this->Components->disable('Security');
+			
+		return parent::beforeFilter();
+	}
+		
+	/**
 	 * Lista de pagamentos
 	 */
 	public function admin_index() {
@@ -29,6 +39,41 @@ class PaymentsController extends AppController {
 		
 			'data' => $this->paginate('Payment')
 		));
+	}	
+		
+	/**
+	 * Sincroniza os dados de pagamento do PagSeguro
+	 */
+	public function syncPayment() {
+		if (!$this->request->is('post'))
+			exit;
+
+		App::import('Vendor', 'PagSeguro', array('file' => 'PagSeguroLibrary' . DS . 'PagSeguroLibrary.php'));
+
+		$AccountCredentials = new AccountCredentials(Configure::read('PagSeguro.API.email'), Configure::read('PagSeguro.API.token'));
+
+		$this->log('POST: ' . serialize($_POST), 'PagSeguro');
+		$this->log('Data: ' . serialize($this->request->data), 'PagSeguro');
+
+		/* Tipo de notificação recebida */  
+		$type = $_POST['notificationType'];  
+
+		/* Código da notificação recebida */  
+		$code = $_POST['notificationCode'];
+
+		/* Verificando tipo de notificação recebida */  
+		if ($type === 'transaction') {  
+
+			/* Obtendo o objeto PagSeguroTransaction a partir do código de notificação */  
+			$transaction = PagSeguroNotificationService::checkTransaction(  
+				$AccountCredentials,  
+				$code // código de notificação  
+			); 
+
+			$this->log('Transaction: ' . serialize($transaction), 'PagSeguro');
+
+		}
+
 	}
 	
 }
