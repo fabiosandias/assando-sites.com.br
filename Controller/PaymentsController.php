234aef5d69cd7261211e9c77b69c23868e7968dc
Thiago Belem <contato@thiagobelem.net>
2012-02-11 21:51:54 -0200

    Update Controller/PaymentsController.php

--- Controller/PaymentsController.php
+++ Controller/PaymentsController.php
@@ -67,20 +67,109 @@ class PaymentsController extends AppController {
 				$AccountCredentials,  
 				$transactionCode
 			);
+			
+			if (empty($Transaction)) {
+				$this->log('Transação não encontrada: ' . $transactionCode, 'PagSeguro');
+				exit;
+			}
 
-			$body = var_export($Transaction, true);
+			$reference = $Transaction->getReference();
+			
+			// Se não tem referência, pula
+			if (empty($reference))
+				exit;
+			
+			// Encontra o aluno
+			$Student = $this->Payment->Student->field('id', array('SHA1(Student.email)' => $reference));
+			
+			// Se não tem aluno, pula
+			if (empty($Student))
+				exit;
+			
+			// Enconta o pagamento pelo código e pelo ID do aluno (que corresponde ao reference)
+			$Payment = $this->Payment->find('first', array(
+				'fields' => array('id', 'updated', 'status_id'),
+				'conditions' => array(
+					'code' => $Transaction->getCode(),
+					'student_id' => $Student
+				)
+			));
+			
+			// Dados à serem salvos
+			$data = array(
+				'id' => !empty($Payment) ? $Payment['Payment']['id'] : null,
+				'student_id' => !empty($Student) ? $Student : 0,
+	
+				'code' => $Transaction->getCode(),
+				'reference' => !empty($reference) ? $reference : '',
+	
+				'value' => $Transaction->getGrossAmount(),
+				'datetime' => $Transaction->getDate(),
+	
+				'payment_gateway_id' => PAYMENT_GATEWAY_PAGSEGURO,
+				'payment_method_id' => $Transaction->getPaymentMethod()->getType()->getValue(),
+				
+				'status_id' => max(11, 10 + $Transaction->getStatus()->getValue()),
+				
+				'updated' => $Transaction->getLastEventDate()
+			);
+			
+			// Salva ou atualiza os dados do pagamento
+			if ($this->Payment->save($data)) {
 
-			$CakeEmail = new CakeEmail('smtp');
-			$CakeEmail->to('contato@thiagobelem.net')
-					  ->from('contato@thiagobelem.net')
-					  ->subject('[PagSeguro] - Transaction: ' . $transactionCode)
-					  ->emailFormat('text')
-					  ->send($body);
+				$newPayment = (bool)empty($Payment);
+				$oldStatus = !empty($Payment) ? (int)$Payment['Payment']['status_id'] : false;
+				$newStatus = (int)$data['status_id'];
 
-			$this->log('Transaction Object: ' . serialize($Transaction), 'PagSeguro');
+				// Se o status foi alterado pra PAGO
+				if (($newStatus == STATUS_PAYMENT_PAGO) && ($newPayment || ($oldStatus != $newStatus))) {
+					// Atualiza o status do aluno
+					$this->Payment->Student->id = (int)$Student;
+					if ($this->Payment->Student->saveField('status_id', STATUS_STUDENT_INSCRICAO_CONFIRMADA)) {
+						// Envia o email de confirmação
+						$this->__confirmPayment($this->Payment->id);
+					}
+				}
+			}
 
 		}
 
+	}
+	
+	/**
+	 * Confirma o pagamento de um aluno
+	 */
+	protected function __confirmPayment($payment_id) {
+		$this->log('Confirmando pagamento #' . $payment_id, 'payments');
+
+		// Encontra os dados do pagamento, do aluno e das turmas
+		$PaymentObject = $this->Payment->find('first', array(
+			'conditions' => array('Payment.id' => $payment_id),
+			'contain' => array(
+				'PaymentGateway',
+				'Student' => array('MyClass')
+			)
+		));
+
+		extract($PaymentObject);
+
+		if (empty($Payment) OR empty($Student['id'])) {
+			$this->log('Pagamento #' . $payment_id . ' não encontrado', 'payments');
+			exit;
+		}
+		
+		$Payment['PaymentGateway'] = $PaymentGateway;
+		
+		$this->EmailQueue->to = array($Student['fullname'] => $Student['email']);
+		$this->EmailQueue->bcc = Configure::read('Email.from');
+		
+		$this->EmailQueue->subject = 'Assando Sites - Sua vaga está garantida!';
+		
+		$this->EmailQueue->view = 'payment_confirmation';
+		$this->EmailQueue->set('Student', $Student);
+		$this->EmailQueue->set('Payment', $Payment);
+		
+		$this->EmailQueue->queue(); exit;
 	}
 	
 }
